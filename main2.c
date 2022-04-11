46c5652ec50b28c9f6bded39c69c8dc73092325f
Sahil_Baori <sahilbaori2@gmail.com>
2022-04-11 21:33:14 +0530

    Added adj_matrix display

--- main2.c
+++ main2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#define heap_ptr_size 22
+#define heap_ptr_size 52
 #define header_size 2
 #define null -2
 int heap_ptr[heap_ptr_size]; //heap
-int free_list_start=0;         // start pointer of the free list (points at the start of the memory by default).
+int free_list_start=0;       // start pointer of the free list (points at the start of the memory by default).
 
 // allocated blocks have the head pointer pointing to 0 by default.
 
@@ -67,10 +67,9 @@ void merge(int prev_ptr,int next_ptr,int ptr)
 {
 
     /* Here we are first merging the current block to the next free block(or adding to the free list) then we merge(or add) the newly added (or merged)
-       block with the previous free block 
+       block with the previous free block
 
     */
-   
     // head pointer of current block will need to be pointed to the next free block no matter what.
 	heap_ptr[ptr+1] = next_ptr;
 
@@ -96,7 +95,6 @@ void merge(int prev_ptr,int next_ptr,int ptr)
 			heap_ptr[prev_ptr+1] = heap_ptr[ptr+1];
             ptr = prev_ptr;
 		}
-		
 	}
 	initialize_mem(ptr,heap_ptr[ptr],0);
 }
@@ -151,9 +149,7 @@ int mem_alloc(int size)
 			addr_prev=i;
 			i = heap_ptr[i+1];
 		}while(i!=null);
-    
     //if we have found one, update the free list.
-	
     if(addr_min!=-3)
 		addr_min = update_offsets_alloc(addr_min,addr_min_prev,size);
 	}
@@ -177,27 +173,50 @@ void display_heap_layout()
             // j++;
             printf("-");
         }
-			
 		i++;
- 
 	}while(i<heap_ptr_size);
 	printf("\n");
 
 }
+void display_matrix(int adj_matrix[][10])
+{
+	for(int i=0;i<10;i++)
+	{
+		for(int j=0;j<10;j++)
+			printf(" %d :(%d,%d)|",adj_matrix[i][j],i,j);
+		printf("\n");
+	}
+}
 int main()
 {
+	int adj_matrix[10][10];
+	int root_set[4];
+	int sim_struct_size[3] = {2,3,4};
 	initialize_mem(free_list_start,heap_ptr_size,null);
-      int a = mem_alloc(2);
-      printf("%d\n",a);
-      int b = mem_alloc(3);
-       int c = mem_alloc(1);
-      int d = mem_alloc(5);
-       free_mem_alloc(a);
-     free_mem_alloc(c);
-      int e = mem_alloc(1);
-       free_mem_alloc(d);
-      free_mem_alloc(e);
-       free_mem_alloc(b);
+     	int alloc[11];
+	alloc[5] = mem_alloc(sim_struct_size[0]);
+      	alloc[1] = mem_alloc(sim_struct_size[2]);
+	heap_ptr[alloc[5]+1] = alloc[1];
+       	alloc[2] = mem_alloc(sim_struct_size[0]);
+	heap_ptr[alloc[1]+1] = alloc[2];
+	heap_ptr[alloc[2]+1] = -3;
+      	alloc[7] = mem_alloc(sim_struct_size[1]);
+	heap_ptr[alloc[7]+1] = alloc[1];
+	alloc[3] = mem_alloc(sim_struct_size[1]);
+	alloc[8] = mem_alloc(sim_struct_size[0]);
+	heap_ptr[alloc[3]+1] = alloc[8];
+	heap_ptr[alloc[7]+2] = alloc[8];
+	alloc[9] = mem_alloc(sim_struct_size[0]);
+	heap_ptr[alloc[8]+1] = alloc[9];
+	alloc[10] = mem_alloc(sim_struct_size[0]);
+	heap_ptr[alloc[1]+2] = alloc[9];
+	heap_ptr[alloc[1]+3] = alloc[10];
+	heap_ptr[alloc[3]+2] = alloc[10];
+/*     	free_mem_alloc(c);
+      	int e = mem_alloc(1);
+       	free_mem_alloc(d);
+      	free_mem_alloc(e);
+       	free_mem_alloc(b);*/
 	display_heap_layout();
 return 0;
 }
