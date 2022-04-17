41defe255cefba8b492782e5412db8147e5931a9
Sahil_Baori <sahilbaori2@gmail.com>
2022-04-17 22:45:25 +0530

    Reference count, previous commit bugs

--- main2.c
+++ main2.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define heap_ptr_size 500
-//#define header_size 2
-#define null -2
 char heap_ptr[heap_ptr_size]; // heap
-							  //  start pointer of the free list (points at the start of the memory by default).
 
-// allocated blocks have the head pointer pointing to 0 by default.
 
+// HEADERS for each allocated/free blocks in heap.
 struct header
 {
 	short int size;
@@ -18,19 +15,20 @@ struct header
 	struct header *next_free;
 };
 
+// The nodes (given in question diagram) to be allocated.
 struct allocs
 {
 	char tag;
 	char* ptr[3];
 };
-int header_size = sizeof(struct header);
-struct header *free_list_start;
-struct header *first_head;
+int header_size = sizeof(struct header);	// Defining the header size.
+struct header *free_list_start; 			//  start pointer of the free list (points at the start of the memory by default)
+struct header *first_head;      			// Start of the header blocks.
 
 // initialize the memory(heap)
-void initialize_mem(int size, int next_ptr, struct header *head)
+void initialize_mem(int size, struct header *head)
 {
-	if (size && next_ptr)
+	if (size)
 	{
 		head->size = size;
 		head->ref_count = 0;
@@ -73,6 +71,7 @@ struct header *update_offsets_alloc(struct header *addr_curr, struct header *add
 		else
 			head_prev->next = alloced;
 		alloced->alloc = 1;
+		alloced->ref_count=0;
 		alloced->next_free = NULL;
 	}
 	return alloced;
@@ -89,6 +88,7 @@ void merge(struct header *prev_ptr, struct header *next_ptr, struct header *targ
 	// head pointer of current block will need to be pointed to the next free block no matter what.
 	target_block->next_free = next_ptr;
 	target_block->alloc = 0;
+	target_block->ref_count=0;
 	// heap_ptr[ptr+1] = next_ptr;
 
 	// check if the nearest right free block is adjacent
@@ -120,6 +120,7 @@ void merge(struct header *prev_ptr, struct header *next_ptr, struct header *targ
 			prev_ptr->next_free = target_block->next_free;
 			free(temp);
 		}
+		prev_ptr->ref_count=0;
 	}
 }
 void free_mem_alloc(void *ptr)
@@ -195,6 +196,7 @@ void *mem_alloc(short int size)
 	return (addr + header_size);
 }
 
+// Populates the adjacency matrix for a given root.
 void populate_matrix(int adj_matrix[][11],char *root,struct allocs chunks[])
 {
 	char i=*root;
@@ -202,12 +204,66 @@ void populate_matrix(int adj_matrix[][11],char *root,struct allocs chunks[])
 	while(chunks[i].ptr[k]!=NULL)
 	{
 		adj_matrix[i][*(chunks[i].ptr[k])] = 1;
-		display_matrix(adj_matrix,chunks[i].ptr[k],chunks);
+		populate_matrix(adj_matrix,chunks[i].ptr[k],chunks);
 		k++;
 	}
 
 }
 
+// Updates the reference to the memory chunk in heap
+void update_reference(char *ptr,struct header* head,int flag)
+{
+	ptr-=header_size;
+	while (head != NULL && head->h_ptr!=ptr)
+	{
+		head = head->next;
+	}
+	if(head!=NULL)
+	{
+		// If flag = 1, a reference needs to be added.
+		if(flag)
+			head->ref_count++;
+		// else, a reference was taken away from the chunk.
+		else
+			head->ref_count--;
+	}
+	else
+		printf("Not a valid pointer!\n");
+}
+
+void gc_reference_count(struct header* head,struct allocs chunks[])
+{
+	int done=0;
+	struct header* temp;
+
+	// Iterates through the heap in constant space (single stack frame) as long as there are any reference changes in the heap.
+	while(!done)
+	{
+		done = 1;
+		temp = head;
+		while(temp!=NULL)
+		{
+			if(temp->ref_count==0 && temp->alloc==1)
+			{
+			 	char *ptr = temp->h_ptr;
+			 	int tag = *(ptr+header_size);
+				int k=0;
+				while(chunks[tag].ptr[k]!=NULL)
+				{
+					update_reference((chunks[tag].ptr[k]),head,0);
+					chunks[tag].ptr[k]=NULL;
+					k++;
+					done=0;
+				}
+				*(ptr+header_size)=0;
+				printf("tag: %d Freed!\nBytes freed: (Header size)%d + (Payload)%d\n\n",tag,header_size,sizeof(struct allocs));
+				free_mem_alloc(ptr+header_size);
+			}
+			temp=temp->next;
+		}
+	}
+
+}
 int main()
 {
 	int adj_matrix[11][11];
@@ -216,82 +272,118 @@ int main()
 		for(int j=0;j<11;j++)
 			adj_matrix[i][j] = 0;
 	}
-	// int root_set[4];
-	// int sim_struct_size[3] = {2,3,4};
-	// struct header* free_start = malloc(sizeof(struct header));
-	free_list_start = malloc(sizeof(struct header));
-	first_head = malloc(sizeof(struct header));
-	initialize_mem(heap_ptr_size, null, free_list_start);
-	first_head->h_ptr = heap_ptr;
-	first_head->next = NULL;
-	first_head->size = free_list_start->size;
-	struct allocs chunks[10];
 
+	free_list_start = malloc(sizeof(struct header));
+	
+	initialize_mem(heap_ptr_size, free_list_start);
+	first_head = free_list_start;
+
+	// The chunks to be allocated from the given diagram.
+	struct allocs chunks[10];
+	
+
+	// Initializing.
 	for(int i=0;i<10;i++)
 	{
-		//chunks[i].tag=0;
 		chunks[i].ptr[0]=NULL;
 		chunks[i].ptr[1]=NULL;
-		chunks[i].ptr[3]=NULL;
+		chunks[i].ptr[2]=NULL;
 	}
+
+	// Allocating all the chunks with their tags. A separate 1 byte is reserved in the heap for the tag at the start of the chunk.
 	char *root1 = mem_alloc(sizeof(struct allocs));
-	char *root2 = mem_alloc(sizeof(struct allocs));
 	*root1 = 5;
+	update_reference(root1,first_head,1);
+
+	char *root2 = mem_alloc(sizeof(struct allocs));
 	*root2 = 1;
+	update_reference(root2,first_head,1);
+	
 	char *two = mem_alloc(sizeof(struct allocs));
 	*two = 2;
-	
+	update_reference(two,first_head,1);
 	
 	char *nine = mem_alloc(sizeof(struct allocs));
 	*nine = 9;
-	
+	update_reference(nine,first_head,1);
 
 	char* ten = mem_alloc(sizeof(struct allocs));
 	*ten = 10;
-	
+	update_reference(ten,first_head,1);
 
 	char *eight = mem_alloc(sizeof(struct allocs));
 	*eight = 8;
-	
+	update_reference(eight,first_head,1);
 
 	char *seven = mem_alloc(sizeof(struct allocs));
 	*seven = 7;
+	update_reference(seven,first_head,1);
 
 	char *three = mem_alloc(sizeof(struct allocs));
 	*three = 3;
-	
-	chunks[5].ptr[0] = root2;
-	chunks[1].ptr[0] = two;
-	chunks[1].ptr[1] = nine;
-	chunks[1].ptr[2] = ten;
-	
-	chunks[8].ptr[0] = nine;
-	
-	chunks[7].ptr[0] = root2;
-	chunks[7].ptr[1] = eight;
+	update_reference(three,first_head,1);
 
-	chunks[3].ptr[0] = eight;
-	chunks[3].ptr[1] = ten;
+	
+	// Creating the links between the chunks from the allocated chunks.
+	chunks[5].ptr[0] = root2;
+	update_reference(root2,first_head,1);
+	
+	chunks[1].ptr[0] = two;
+	update_reference(two,first_head,1);
+
+	chunks[1].ptr[1] = nine;
+	update_reference(nine,first_head,1);
+
+	chunks[1].ptr[2] = ten;
+	update_reference(ten,first_head,1);
+
+	chunks[8].ptr[0] = nine;
+	update_reference(nine,first_head,1);
+
+	chunks[7].ptr[0] = root2;
+	update_reference(root2,first_head,1);
+
+	 chunks[7].ptr[1] = eight;
+	update_reference(eight,first_head,1);
+
+
+	 chunks[3].ptr[0] = eight;
+	 update_reference(eight,first_head,1);
+
+	 chunks[3].ptr[1] = ten;
+	update_reference(ten,first_head,1);
+
+
+	// Deleting all the references from the nodes to create the linked list structure as given in the question diagram.
+	update_reference(two,first_head,0); 
+	update_reference(nine,first_head,0); 
+	update_reference(eight,first_head,0);
+	update_reference(seven,first_head,0);
+	update_reference(three,first_head,0);
+	update_reference(ten,first_head,0);
 	two = nine = eight = seven = three = ten = NULL;
 
-	populate_matrix(adj_matrix,root2,chunks);
-	for(int i=0;i<11;i++)
-	{
-		for(int j=0;j<11;j++)
-		{
-			printf("%d,(%d,%d) |",adj_matrix[i][j],i,j);
-		}
-		printf("\n");
-	}
+	//gc_reference_count(first_head,chunks);
+
+	// populate_matrix(adj_matrix,root1,chunks);
+	// for(int i=0;i<11;i++)
+	// {
+	// 	for(int j=0;j<11;j++)
+	// 	{
+	// 		printf("%d,(%d,%d) |",adj_matrix[i][j],i,j);
+	// 	}
+	// 	printf("\n");
+	// }
 	// printf("%d",adj_matrix[5][1]);
 	//gc_collect();
 	// struct header *temp_head = first_head;
-	// while (temp_head != NULL)
-	// {
-	// 	printf("size: %d\nptr: %d\nheap: %d\nalloc: %d\n\n", temp_head->size, temp_head->h_ptr, heap_ptr, temp_head->alloc);
-	// 	temp_head = temp_head->next;
-	// }
-	// printf("%d", free_list_start->h_ptr);
+	
+	while (first_head != NULL)
+	{
+		char *c = first_head->h_ptr+header_size;
+		printf("Tag: %d\nsize: %d\nptr: %d\nheap: %d\nrefcount: %d\n\n", *c,first_head->size, first_head->h_ptr, heap_ptr, first_head->ref_count);
+		first_head = first_head->next;
+	}
 	
 	return 0;
 }
